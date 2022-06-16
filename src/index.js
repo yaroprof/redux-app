728dce0fafe6803c082251c6f3cf0b2d9e54fb33
Yaroslav <crosskyiv@gmail.com>
2022-06-16 16:22:57 +0300

    adding buttons: increment/decrement

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './styles.css'
-import { createStore } from './createStore'
+//import { createStore } from './createStore'
+import { applyMiddleware, createStore } from 'redux'
 import {rootReducer} from './redux/rootReducer'
+//import { DECREMENT, INCREMENT } from './redux/types'
+import { decrement, increment, asyncIncrement } from './redux/actions'
+import thunk from 'redux-thunk'
+
 
 const counter = document.getElementById('counter')
 const addBtn = document.getElementById('add')
@@ -8,21 +13,50 @@ const subBtn = document.getElementById('sub')
 const asyncBtn = document.getElementById('async')
 const themBtn = document.getElementById('theme')
 
+// create MiddleWare - do manually
+// logger - базовий елемент MiddleWare
+function logger(state){
+  return function(next){
+    return function(action){
+      console.log('Prev-State', state.getState());
+      console.log('Action', action);
+      const newState = next(action)
+      console.log('New-State', state.getState());
+      return newState
+    }
+  }
+}
+
 // create Redux
 // create store
-const store = createStore(rootReducer, 0)
-
+// 01 - в store ми вказуємо конкретні події на сторніці
+// dispatch - провідник, диспетчер, який передає вхідну інфу - подію в store
+const store = createStore(
+  rootReducer, 
+  45, 
+  applyMiddleware(thunk, logger)  // logger - additional middleWare- added manual. In this case logger need to check all of the - state/ action, abowe
+  )
+// buttons - increment/decrement
 addBtn.addEventListener('click', () => {
-  store.dispatch({type: 'Increment'})
+  store.dispatch(increment())
 })
 subBtn.addEventListener('click', () => {
-  store.dispatch({type: 'Decrement'})
+  store.dispatch(decrement())
 })
-//asyncBtn.addEventListener('click', () =>{
-//  setTimeout(() =>{
-//  }, 2000)
+
+// button - async
+asyncBtn.addEventListener('click', () =>{
+  setTimeout(() =>{
+    store.dispatch(asyncIncrement())
+  }, 2000)
+})
+
+
+//themBtn.addEventListener('click', () =>{
+//  //document.body.classList.toggle('dark')
 //})
 
+// 02 -store за допом. subscribe - дає команду на виконання: передає чіткі вказівки getState- звернись до counter і створи елемент з новим - state. getState робить ревью оновленого стану.
 store.subscribe(() => {
   const state = store.getState()
   counter.textContent = state
@@ -32,17 +66,6 @@ store.dispatch({type: 'ININT_APPLICATION'})
 
 
 
-
-
-
-
-
-
-
-
-//themBtn.addEventListener('click', () =>{
-//  //document.body.classList.toggle('dark')
-//})
 
 
 
---
+++ src/redux/actions.js
@@ -0,0 +1,19 @@
+import {  DECREMENT, INCREMENT } from "./types";
+// actions потрібно для того, щоб type - вказівка на дію не вказувати в index.js
+export function increment(){
+	return{
+		type: INCREMENT
+	}
+}
+
+export function decrement(){
+	return{
+		type: DECREMENT
+	}
+}
+// all async actions create in this place
+export function asyncIncrement(){
+	return function(dispatch){
+		dispatch((increment()), 1500)
+	}
+}
